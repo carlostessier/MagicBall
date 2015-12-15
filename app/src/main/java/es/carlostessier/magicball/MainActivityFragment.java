2aafacad3f0c8b30c06229b987bdcbeb98fbc840
carlosfernandez <prof.carlos.tessier@gmail.com>
2015-12-15 18:53:40 +0100

    add listener

--- app/src/main/java/es/carlostessier/magicball/MainActivityFragment.java
+++ app/src/main/java/es/carlostessier/magicball/MainActivityFragment.java
@@ -1,10 +1,11 @@
 package es.carlostessier.magicball;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,9 +15,34 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        setButtonListener(rootView);
+
+        return rootView;
+    }
+
+    /**
+     * Method that add a Listener to the button prediction
+     * @param rootView
+     */
+    private void setButtonListener(View rootView) {
+        Button button = (Button) rootView.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //TODO obtain prediction
+
+            }
+        });
     }
 }
